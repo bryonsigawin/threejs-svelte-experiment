9bbcc3d7e02c64baed7e1ef84ee777a1cfe3fc58
Bryon Sigawin <zh3nlong@gmail.com>
2023-05-18 23:50:37 +0800

    feat: configure seo stuff

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,14 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
 
+    csrf: {
+      checkOrigin: true
+    },
+
+    csp: {
+      mode: 'auto'
+    },
+
     /**
      * Temporary while this fix gets pushed
      * https://github.com/threlte/threlte/issues/374
